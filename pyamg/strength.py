0ce76952a573248a168619117d7e31c3bb768a45
rsmedleystevenson <rsmedleystevenson@users.noreply.github.com>
2018-11-14 08:56:25 +0000

    Update strength.py

--- pyamg/strength.py
+++ pyamg/strength.py
@@ -242,14 +242,14 @@ def classical_strength_of_connection(A, theta=0.25, block=None, norm='abs', cost
         S_colinds = np.empty_like(A.indices)
         S_data = np.empty_like(data)
         print(np.version.version)
-        help(N)
-        help(theta)
-        help(A.indptr)
-        help(A.indices)
-        help(data)
-        help(S_rowptr)
-        help(S_colinds)
-        help(S_data)
+        print(N)
+        print(theta)
+        print(A.indptr.shape(),A.indptr.dtype())
+        print(A.indices.shape(),A.indices.dtype())
+        print(data.shape(),data.dtype())
+        print(S_rowptr)
+        print(S_colinds)
+        print(S_data)
 
         if norm == 'abs' or norm == 'fro':
             amg_core.classical_strength_of_connection_abs(N, theta, A.indptr, A.indices, data,
